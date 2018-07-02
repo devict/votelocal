f65c929aa83bc8b3af6f3f07070302603adb12e1
Seth Etter <sethetter@gmail.com>
2018-07-02 08:33:08 -0500

    Actually force https in production?

---
+++ app/Http/Middleware/UseHttps.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class UseHttps {
+    public function handle($request, Closure $next)
+    {
+        if (!$request->secure() && env('APP_ENV') === 'production') {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request); 
+    }
+}
