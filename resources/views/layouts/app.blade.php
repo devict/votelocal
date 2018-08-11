e33768e032727e3926c9c1d1e8a8ad33114add39
Christian Taylor <christianbtaylor@gmail.com>
2018-08-11 06:46:13 -0500

    Add logo & favicons (#23)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,15 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ url('site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ url('safari-pinned-tab.svg') }}" color="#ee215a">
+    <meta name="msapplication-TileColor" content="#ee215a">
+    <meta name="theme-color" content="#ffffff">
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
