ba7d3625d3e4869d9e0141eb6ea31b56427993ce
Christian Taylor <christianbtaylor@gmail.com>
2019-07-26 16:19:45 -0500

    Add character count component

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,11 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component(
+    'character-count',
+    require('./components/CharacterCount').default
+);
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
 });
---
+++ resources/js/components/CharacterCount.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <slot></slot>
+        <slot name="label" :limit="limit" :remaining="remaining" :count="count">
+            <small class="text-muted">{{ count }} characters</small>
+        </slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['limit'],
+
+    data: () => ({
+        count: 0,
+        listener: null,
+    }),
+
+    mounted() {
+        this.listener = event => {
+            this.count = event.target.value.length;
+        };
+
+        this.$slots.default[0].elm.addEventListener('input', this.listener);
+    },
+
+    destroyed() {
+        this.$slots.default[0].elm.removeEventListener(this.listener);
+    },
+
+    computed: {
+        remaining() {
+            return this.limit - this.count;
+        },
+    },
+
+    methods: {
+        updateCount(value) {
+            this.count = String(value).length;
+        },
+    },
+};
+</script>
