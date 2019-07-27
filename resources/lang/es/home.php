b092c38cfdfcdfec205f8379018303b7c97a3a13
Seth Etter <mail@sethetter.com>
2019-07-27 08:10:08 -0500

    Add language support notes to homepage

--- resources/lang/es/home.php
+++ resources/lang/es/home.php
@@ -9,8 +9,10 @@ return [
     */
 
     'tagline' => 'Te ayuda a mantenerte comprometido <strong>localmente</strong>.',
-    'text_subscribe_cta' => 'Texto SUSCRIBIR a 864-ICT-VOTE!',
+    'text_subscribe_cta' => 'Texto INICIAR a <a href=":link">864-ICT-VOTE</a>',
     'add_to_contacts_cta' => '..and asegúrese de <a href=":link">agregar Vote ICT a sus contactos</a>!',
+    'locale_support_head' => 'Spanish language available!',
+    'locale_support_body' => 'Envía <strong>ESPAÑOL</strong> en cualquier momento para cambiar tu preferencia, e <strong>INGLÉS</strong> para volver a cambiar.',
     'partners_head' => 'Presentado por la comunidad',
     'partners_body' => 'VoteICT es creado y operado por organizaciones comunitarias locales <a href="https://kmuw.org" title="KMUW">KMUW</a> y <a href="https://devict.org" title="devICT">devICT</a>.'
 
