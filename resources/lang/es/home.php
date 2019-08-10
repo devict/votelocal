275a83735780b033b60329f0792e7f1f278163c9
Seth Etter <mail@sethetter.com>
2019-08-09 21:29:46 -0500

    Updates to Spanish home content

--- resources/lang/es/home.php
+++ resources/lang/es/home.php
@@ -8,9 +8,9 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'tagline' => 'Te ayuda a mantenerte comprometido <strong>localmente</strong>.',
-    'text_subscribe_cta' => 'Texto INICIAR a <a href=":link">864-ICT-VOTE</a>',
-    'add_to_contacts_cta' => '..and asegúrese de <a href=":link">agregar Vote ICT a sus contactos</a>!',
+    'tagline' => 'Ayudandole a participar <strong>localmente</strong>.',
+    'text_subscribe_cta' => 'Para empezar envie la palabra INICIAR a <a href=":link">864-ICT-VOTE</a>',
+    'add_to_contacts_cta' => '..y asegúrese de <a href=":link">agregar Vote ICT a sus contactos</a>!',
     'locale_support_head' => 'Español apoyado!',
     'locale_support_body' => 'Envía <strong>ESPAÑOL</strong> en cualquier momento para cambiar tu preferencia, e <strong>INGLÉS</strong> para volver a cambiar.',
     'partners_head' => 'Presentado por la comunidad',
