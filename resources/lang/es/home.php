bb5d55f1e079669a34e2d4162b3fb1bbb6c83428
Benny Bowden <itsbowden@gmail.com>
2019-10-31 14:20:57 -0500

    Add Wichita to homepage copy

--- resources/lang/es/home.php
+++ resources/lang/es/home.php
@@ -8,12 +8,12 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'intro' => 'Bienvenido a voteict.org',
-    'tagline' => 'Recibe mensajes de texto con información importante sobre nuestras elecciones.',
+    'intro' => 'Bienvenido a VoteICT.org',
+    'tagline' => 'Recibe mensajes de texto con información importante sobre nuestras elecciones en Wichita, KS.',
     'text_to' => 'Envía en texto la palabra INICIAR al 864-ICT-VOTE',
     'learn_more' => 'Aprende más',
     'step_1' => 'Envía en texto la palabra <strong>INICIAR</strong> al <strong><a href=":link">864-ICT-VOTE</a></strong> desde tu celular. (864-428-8683)',
-    'step_2' => 'Mantente informado en cosas como registración para votar, elecciones, candidatos y&nbsp;más.',
+    'step_2' => 'Mantente informado en cosas como Wichita registración para votar, elecciones, candidatos y&nbsp;más.',
     'step_3' => '¡Nunca ha sido más fácil estar activo e involucrado en el&nbsp;proceso!',
     'text_subscribe_keyword' => 'INICIAR',
     'add_to_contacts_cta' => '..y asegúrese de <a href=":link">agregar Vote ICT a sus contactos</a>!',
