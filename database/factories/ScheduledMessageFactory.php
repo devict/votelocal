455ec73800b19e1e69ff4ddc19e5e433d204707a
Christian Taylor <christianbtaylor@gmail.com>
2020-02-29 18:37:45 -0600

    Add required target fields to tests

--- database/factories/ScheduledMessageFactory.php
+++ database/factories/ScheduledMessageFactory.php
@@ -7,7 +7,8 @@ $factory->define(App\ScheduledMessage::class, function (Faker $faker) {
     return [
         'body_en' => $faker->text(160),
         'body_es' => $faker->text(160),
-        'target_sms' => true,
+        'target_sms' => 1,
+        'target_twitter' => 1,
         'send_at' => Carbon::create()->addMinutes(1)->toDateTimeString(),
         'sent' => false,
     ];
