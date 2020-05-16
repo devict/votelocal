068c92406a3abc4e5b6f7d774399dcc16ca80db9
Seth Etter <mail@sethetter.com>
2020-05-15 22:01:48 -0500

    Test fix

--- tests/Feature/ScheduledMessagesTest.php
+++ tests/Feature/ScheduledMessagesTest.php
@@ -83,7 +83,7 @@ class ScheduledMessagesTest extends TestCase
                 'send_at' => $message->send_at,
             ]);
 
-        $request->assertRedirect('/admin/scheduled_messages');
+        $request->assertRedirect('/admin/scheduled_messages/' . $message->id);
 
         $this->assertDatabaseHas('scheduled_messages', [
             'id' => $message->id,
