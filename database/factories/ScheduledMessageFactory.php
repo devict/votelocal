59f2be6e166f0c0b3a53f3b7c278266189dcb913
Seth Etter <mail@sethetter.com>
2020-05-15 21:58:04 -0500

    Factory data fix

--- database/factories/ScheduledMessageFactory.php
+++ database/factories/ScheduledMessageFactory.php
@@ -7,7 +7,8 @@ $factory->define(App\ScheduledMessage::class, function (Faker $faker) {
     return [
         'body_en' => $faker->text(160),
         'body_es' => $faker->text(160),
-        'send_at' => Carbon::create()->addMinutes(2)->toIso8601String(),
+        'target_sms' => true,
+        'send_at' => Carbon::create()->addMinutes(1)->toDateTimeString(),
         'sent' => false,
     ];
 });
