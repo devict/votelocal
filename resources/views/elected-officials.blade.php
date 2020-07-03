ab740f51a6e9705cce546f6dd87ac15ba8f71127
Jake Suellentrop <jake@lifeboatcreative.com>
2020-07-02 23:39:40 -0500

    add explanation paragraph

--- resources/views/elected-officials.blade.php
+++ resources/views/elected-officials.blade.php
@@ -1,20 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<form action="{{ route('elected-officials.lookup') }}" method="post" class="flex justify-center mx-auto w-1/3">
+<header class="mx-auto my-8 w-1/2">
+    <p>For any U.S. residential address, you can look up who represents that address at each elected level of government. During supported elections, you can also look up polling places, early vote location, candidate data, and other election official information.</p>
+    <p class="mt-6">Enter your address below to search.</p>
+</header>
+
+<aside class="mx-auto w-1/2">
+    <p class="text-right text-xs">Powered by Google Civic Information API</p>
+</aside>
+
+<form action="{{ route('elected-officials.lookup') }}" method="post" class="flex justify-center mt-6 mx-auto w-1/3">
     @csrf
     <input type="text" name="address" id="address" class="border border-black m-4">
     <button type="submit">Submit</button>
 </form>
 
 @if (isset($data))
-<h3 class="text-center text-xl">Showing results for address:</h3>
+<h3 class="font-bold font-display font-medium mt-12 text-center text-2xl">Showing results for address:</h3>
 @php
     $address = $data->normalizedInput
 @endphp
 <p class="text-center">{{ $address->line1 }}</p>
 <p class="text-center">{{ "{$address->city}, {$address->state} {$address->zip}" }}</p>
-<div class="flex flex-wrap justify-between mx-auto w-2/3">
+<div class="flex flex-wrap justify-between mt-12 mx-auto w-2/3">
     @foreach ($data->offices as $office)
     @php $official = $data->officials[$office->officialIndices[0]] @endphp
     <div class="flex justify-between mb-8 w-2/3 bg-white max-w-md mx-auto px-6 py-6 rounded-lg shadow-lg relative z-10">
@@ -24,7 +33,7 @@
         </div>
         @endif
         <div class="px-4 w-3/4">
-            <p class="font-bold">{{ $office->name }}</p>
+            <p class="font-bold font-display">{{ $office->name }}</p>
             <p>{{ $official->name }}</p>
             <p>{{ $official->party }}</p>
             {{-- <p><a href="tel:{{ $official->"></a></p> --}}
