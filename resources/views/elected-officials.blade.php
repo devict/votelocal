0cb3480cbc74f55c77fc3ee096da85be55caa8b8
Jake Suellentrop <jake@lifeboatcreative.com>
2020-07-02 22:52:38 -0500

    style 'cards' for results

--- resources/views/elected-officials.blade.php
+++ resources/views/elected-officials.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<form action="{{ route('elected-officials.lookup') }}" method="post">
+<form action="{{ route('elected-officials.lookup') }}" method="post" class="flex justify-center mx-auto w-1/3">
     @csrf
     <input type="text" name="address" id="address" class="border border-black m-4">
     <button type="submit">Submit</button>
 </form>
 
 @if (isset($data))
-<div class="flex flex-wrap justify-center mx-auto w-2/3">
+<div class="flex flex-wrap justify-between mx-auto w-2/3">
     @foreach ($data->offices as $office)
     @php $official = $data->officials[$office->officialIndices[0]] @endphp
-    <div class="m-8 flex">
+    <div class="flex mb-8 w-1/2 bg-white max-w-md mx-auto px-10 py-5 rounded-lg shadow-lg relative z-10">
         @if (isset($official->photoUrl))
         <img src="{{ $official->photoUrl }}" alt="{{ $official->name }}" height="100" width="100" class="mx-4">
         @endif
