b8eb5bedb484ed2a7464a96c57539da6407f3ae1
Seth Etter <mail@sethetter.com>
2020-07-05 16:28:51 -0500

    smol thins

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Closure;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -14,4 +15,13 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'sms/receive',
     ];
+
+    public function handle($request, Closure $next)
+    {
+        if (env('APP_ENV') === 'testing') {
+            return $next($request);
+        }
+
+        return parent::handle($request, $next);
+    }
 }
