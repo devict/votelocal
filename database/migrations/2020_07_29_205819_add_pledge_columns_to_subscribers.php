20860334811f964a12902f58d37aebcfe6e34956
Seth Etter <mail@sethetter.com>
2020-07-29 21:20:20 -0500

    Add pledge columns

---
+++ database/migrations/2020_07_29_205819_add_pledge_columns_to_subscribers.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPledgeColumnsToSubscribers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('subscribers', function (Blueprint $table) {
+            $table->boolean('pledged')->default(false);
+            $table->string('referrer_id');
+            $table->string('referred_by')->nullable();
+            $table->boolean('hide_from_pledge_board')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscribers', function (Blueprint $table) {
+            $table->dropColumn('pledged');
+            $table->dropColumn('referrer_id');
+            $table->dropColumn('referred_by');
+            $table->dropColumn('hide_from_pledge_board');
+        });
+    }
+}
