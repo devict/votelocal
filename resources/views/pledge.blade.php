65d2555501bde4a2c2253174fa6e1be9d753a400
Seth Etter <mail@sethetter.com>
2020-08-24 19:08:36 -0500

    Partner logos on pledge page

--- resources/views/pledge.blade.php
+++ resources/views/pledge.blade.php
@@ -33,9 +33,38 @@
                     </div>
                     <button class="btn text-xl font-bold mt-6 w-full">@lang('Pledge to Vote!')</button>
 					<p class="text-sm text-gray-600 text-center mt-2 px-4">
-						@lang('You will also receive important election reminders and voting information.')
+						@lang('You will also receive important election reminders, opt out at any time.')
 					</p>
                 </form>
+
+                <div class="mx-auto mt-6 w-24 border-b-2"></div>
+
+            </div>
+        </div>
+    </div>
+</main>
+
+<main class="my-16 sm:px-6 lg:px-8">
+    <div class="w-full max-w-sm mx-auto">
+        <div class="mt-16 bg-white rounded-lg shadow-lg overflow-hidden">
+            <div class="px-4 py-5 sm:p-6">
+                <h1 class="text-center font-bold text-xl mt-4">
+                    @lang('Brought to you by the Community')
+                </h1>
+                <div>
+                    <a href="https://wyoungpros.com">
+                        <img src="{{ asset('img/w-young-pros.png')}}" alt="@lang('W Young Pros')">
+                    </a>
+                    <a href="https://kmuw.org">
+                        <img src="{{ asset('img/kmuw.png')}}" alt="@lang('KMUW')">
+                    </a>
+                    <a href="https://devict.org">
+                        <img src="{{ asset('img/devict.png')}}" alt="@lang('devICT')">
+                    </a>
+                    <a href="https://www.abcbilingualresources.com">
+                        <img src="{{ asset('img/ab-c-bilingual.png')}}" alt="@lang('AB&C Bilingual Resources')">
+                    </a>
+                </div>
             </div>
         </div>
     </div>
