4a8387dabbdba8de8e0197960dd5778556fbf319
Christian Taylor <christianbtaylor@gmail.com>
2020-09-13 16:32:42 -0500

    Fix checkbox sizing

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -3,6 +3,6 @@
 ])
 
 <label class="inline-flex items-center">
-    <input type="checkbox" {{ $attributes->merge(['class' => 'form-checkbox']) }}>
+    <input type="checkbox" {{ $attributes->merge(['class' => 'form-checkbox w-4 h-4']) }}>
     <span class="ml-2">{{ $label }}</span>
 </label>
