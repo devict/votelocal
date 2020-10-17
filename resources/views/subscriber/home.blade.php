3fb2a9a13a5ccf342b38c7ee4ff4773d504327d6
Seth Etter <mail@sethetter.com>
2020-10-16 23:18:42 -0500

    Locale update from subscriber home

--- resources/views/subscriber/home.blade.php
+++ resources/views/subscriber/home.blade.php
@@ -129,6 +129,17 @@
                                 </form>
                             @endif
                         @endif
+                        <form action="{{ route('subscriber.updateLocale')}}" method="POST" class="mt-{{ $subscriber->pledged ? '4' : '16' }} mb-16">
+                            @csrf
+                            <select name="locale" class="w-1/2 bg-white border border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                                <option value="en" {{ $subscriber->locale === 'en' ? 'selected' : '' }}>@lang('English')</option>
+                                <option value="es" {{ $subscriber->locale === 'es' ? 'selected' : '' }}>@lang('Spanish')</option>
+                            </select>
+                            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                            </div>
+                            <button onclick="return confirm('Are you sure?')" class="btn float-right">@lang('Update Language')</button>
+                        </form>
                         <form action="{{ route('subscriber.disable')}}" method="POST" class="mt-{{ $subscriber->pledged ? '4' : '16' }} mb-16 text-center">
                             @csrf
                             <button onclick="return confirm('Are you sure?')" class="hover:underline text-sm text-red-400">@lang('Disable subscription.')</button>
